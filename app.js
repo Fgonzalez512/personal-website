977c3432f1d2dccf908e3dfcf79a7b428265a931
Frank Gonzalez <frank.gonzalez628@gmail.com>
2016-08-03 18:08:55 -0500

    jquery

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $('#comics-button').on('click', function(event) {
          $('.comics').toggle('show');
     });
+    $('#movies-button').on('click', function(event) {
+         $('.movies').toggle('show');
+    });
+    $('#music-button').on('click', function(event) {
+         $('.music').toggle('show');
+    });
 });
